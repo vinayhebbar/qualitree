70a5c1c5af7bc39623231d5442bac01d7b7264da
vinayhebbar <vinayhebbar@gmail.com>
2017-08-29 19:45:33 +0530

    first
    commit

---
+++ shop/themes/default/views/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+        <div style="text-align:center;"><img src="<?php echo base_url('assets/img/logo1.png');?>" alt="Driven By GoCart"></div>        
+    </footer>
+</div>
+</body>
+</html>
